5fe18c861825f1869d4069a02305e7107f22eabf
Ludmila2023 <gvadalahr@yandex.ru>
2023-04-22 17:58:33 +0300

    homework2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 
 class Item:
     """
@@ -14,10 +16,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
+        # Item.all.append(self.name)
 
     def calculate_total_price(self):
         """
@@ -25,7 +27,7 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        total_price = self.price*self.quantity
+        total_price = self.price * self.quantity
         return total_price
 
     def apply_discount(self):
@@ -33,3 +35,35 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open("../homework-2/items.csv", newline='') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=",")
+            for item in reader:
+                name = item["name"]
+                price = int(item["price"])
+                quantity = int(item["quantity"])
+                # exemp_class = name, price, quantity
+                # print(exemp_class)
+                cls.all.append(name)
+            return name, price, quantity
+
+    @staticmethod
+    def string_to_number(value):
+        return int(float(value))
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(self.__name) < 10:
+            self.__name == name
+        else:
+            print("Exception: Длина наименования товара превышает 10 символов")
+
+
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,14 +1,29 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
-item = Item("Смартфон", 10000, 20)
-# def test_init():
-#     assert Item("Смартфон", 10000, 20) == '<src.item.Item object at 0x00000221107603D0>'
-def test_calculate_total_price():
+import csv
 
+item = Item("Смартфон", 10000, 20)
+
+def test_calculate_total_price():
     assert item.calculate_total_price() == 200000
 
 
 def test_apply_discount():
     assert item.apply_discount() == None
+
+
+# def test_instantiate_from_csv():
+#     assert item.instantiate_from_csv() ==('Клавиатура', 75, 5)
+
+def test_name():
+    assert item.name == 'Смартфон'
+    if item.name == 'СуперСмартфон':
+        assert "Exception: Длина наименования товара превышает 10 символов"
+
+def test_string_to_number():
+    assert Item.string_to_number('6.0') == 6
+
+
+
 
 
